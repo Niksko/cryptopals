1cd839cae34ee979c8fc59836513c25f068800bd
Nikolas M Skoufis <n.skoufis@gmail.com>
2014-10-29 01:56:00 +1100

    Made matching string a buffer instead

--- set1tests.py
+++ set1tests.py
@@ -10,5 +10,5 @@ class Test_Set1(unittest.TestCase):
     def test_hex_to_64(self):
         """Tests the hex to base 64 function on the input provided"""
         test_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-        correct_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        correct_string = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
         self.assertEqual(hex_to_base64(test_string), correct_string)
